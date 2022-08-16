596256b136cfd883e064a042f02cec9757e40bee
pythearn <ermandmani@gmail.com>
2022-08-16 09:43:50 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    { name: "Buy milk", isPriority: false },
+    { name: "Formula1 @10pm", isPriority: true },
+    { name: "Doctor @1pm", isPriority: false },
+  ])
+
+
+  const [newTask, setNewTask] = useState("");
+
+
+  const taskNodes = tasks.map((task, index) => {
+    return (
+      <li key={index} className={task.isPriority ? "priority" : "not-priority"}>
+      <span>{task.name}</span>
+      {task.isPriority? <span className="priority"><button>Done</button></span> : <button onClick={() => priorityTask(index)}>To do</button>}
+      </li>
+    )
+  })
+
+
+  const handleTaskInput = (event) => {
+    setNewTask(event.target.value)
+  }
+
+  const saveNewTask = (event) => { 
+    event.preventDefault();
+    const copyTasks = [...tasks]
+    copyTasks.push({name: newTask, isPriority: false})
+    setTasks(copyTasks)
+    setNewTask("")
+  }
+
+
+  const priorityTask = (index) => {
+    const copyTasks = [...tasks]
+    copyTasks[index].isPriority = true;
+    setTasks(copyTasks)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>To do list</h1>
+
+
+      <ul>
+        {taskNodes}
+      </ul>
+
+      <form onSubmit={saveNewTask}>
+        <label htmlFor="new-task">Add a new task:</label>
+        <input id="new-task" type="text" value={newTask} onChange={handleTaskInput} />
+        <input type="submit" value="Save New Task" />
+      </form>
+
+
+
     </div>
   );
+
 }
 
 export default App;
